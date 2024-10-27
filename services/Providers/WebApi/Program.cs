6e153789c03f35e84c92031b5d1d7b4e96e3922f
ziqushru <jasonkoutoulias@gmail.com>
2024-10-27 23:34:32 +0200

    Fixed RabbitMQ communication

--- services/Providers/WebApi/Program.cs
+++ services/Providers/WebApi/Program.cs
@@ -73,12 +73,14 @@ builder.Services
     })
     .AddMassTransit(busConfigurator =>
     {
+        busConfigurator.SetKebabCaseEndpointNameFormatter();
+        
         busConfigurator.UsingRabbitMq((context, configurator)=>
         {
             configurator.Host(new Uri(builder.Configuration["MessageBroker:Host"]!), h =>
             {
-                h.Username(builder.Configuration["MessageBroken:Username"]!);
-                h.Password(builder.Configuration["MessageBroken:Password"]!);
+                h.Username(builder.Configuration["MessageBroker:Username"]!);
+                h.Password(builder.Configuration["MessageBroker:Password"]!);
             });
             
             configurator.ConfigureEndpoints(context);
